fe8286d47354eeb22742107af65f0bfa61c6b567
sAnjali12 <anjalisen060@gmail.com>
2019-09-27 16:36:29 +0530

    first post route

---
+++ saralRoutes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var bodyParser = require('body-parser')
+var expresapp = app.use(bodyParser.json())
+
+expresapp.post("/post",function(req,res){
+    var user = {
+        name:req.body.name,
+        discription:req.body.discription
+    }
+    id = req.params.id
+    var data = fs.readFileSync("courses.json")
+    data = data.toString();
+    var Data = JSON.parse(data)
+    user.id = Data.length + 1;
+    Data.push(user)
+    fs.writeFileSync("courses.json", JSON.stringify(Data,null,2))
+    return res.json(Data)
+
+})
+
+const port = 2000
+app.listen(port,()=>
+   console.log(`my server is listning port....${port}`)
+);
